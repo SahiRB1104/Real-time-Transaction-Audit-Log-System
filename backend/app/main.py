6f701bb4b552d7f46dcab84578a892483ddf0543
sahil bhalekar <129488267+SahiRB1104@users.noreply.github.com>
2025-12-22 00:10:04 +0530

    cors update

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://real-time-transaction-audit-log-sys.vercel.app/",
+                   "http://localhost:5173",],
     allow_credentials=True,
     allow_methods=["*"],   # VERY IMPORTANT
     allow_headers=["*"],   # VERY IMPORTANT
